d9088c2d43783bb2420751deb9b3296bb90c1a51
Balázs Kiss <dev@balazskiss.name>
2015-02-07 17:05:40 +0000

    Removed unnecessary parts from GUISimulation class

--- src/trafficsimulator/TrafficSimulator.java
+++ src/trafficsimulator/TrafficSimulator.java
@@ -25,12 +25,12 @@ public class TrafficSimulator extends Application {
   @Override
   public void start(Stage primaryStage) {
     Button btn = new Button();
-    btn.setText("Say 'Hello World'");
+    btn.setText("Start Simulation");
     btn.setOnAction(new EventHandler<ActionEvent>() {
       
       @Override
       public void handle(ActionEvent event) {
-        System.out.println("Hello World!");
+        System.out.println("Starting simulation...");
         simulation.start();
       }
     });
@@ -40,7 +40,7 @@ public class TrafficSimulator extends Application {
     
     Scene scene = new Scene(root, 300, 250);
     
-    primaryStage.setTitle("Hello World!");
+    primaryStage.setTitle("TrafficSimulator");
     primaryStage.setScene(scene);
     primaryStage.show();
   }
--- src/trafficsimulator/core/GUIsimulation.java
+++ src/trafficsimulator/core/GUIsimulation.java
@@ -18,6 +18,7 @@ import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import trafficsimulator.core.Lane.Direction;
+import trafficsimulator.simulations.Simulation1;
 import trafficsimulator.utils.Point;
 import trafficsimulator.vehicles.NormalCar;
 
@@ -26,66 +27,14 @@ import trafficsimulator.vehicles.NormalCar;
  * @author yukolthep
  */
 public class GUIsimulation extends Application {
-    static Map m = new Map();
-    
-        static Road r1 = new Road(new Point(0, 0), new Point(100, 0));
-        static Lane l11 = new Lane(Direction.IDENTICAL);
-        static Lane l12 = new Lane(Direction.OPPOSITE);
-        
-        static Road r2 = new Road(new Point(100, 0), new Point(100, 100));
-        static Lane l21 = new Lane(Direction.IDENTICAL);
-        static Lane l22 = new Lane(Direction.OPPOSITE);
-        
-        static Road r3 = new Road(new Point(100, 100), new Point(0, 0));
-        static Lane l31 = new Lane(Direction.IDENTICAL);
-        static Lane l32 = new Lane(Direction.OPPOSITE);
-        
-    
-        static Junction j1 = new Junction();
-        
-        static Junction j2 = new Junction();
-        
-        static Junction j3 = new Junction();
-        
-    
-        
 
-        static NormalCar v1 = new NormalCar(l11, r1.getRandomPosition());
-        static NormalCar v2 = new NormalCar(l21, r2.getRandomPosition());
-        static NormalCar v3 = new NormalCar(l12, r1.getRandomPosition());
         
-        static Simulation s = new Simulation();
+    static Simulation1 s = new Simulation1();
         
-        
-        final static double CAR_WIDTH = 10.0;
-        final static double CAR_HEIGHT = 10.0;
-    
     
     @Override
     public void start(Stage stage) {
         //stage.setResizable(false);
-        r1.addLane(l11);
-        r1.addLane(l12);
-        r2.addLane(l21);
-        r2.addLane(l22);
-        r3.addLane(l31);
-        r3.addLane(l32);
-        j1.connect(l11, l21);
-        j1.connect(l22, l12);
-        j2.connect(l21, l31);
-        j2.connect(l32, l22);
-        j3.connect(l31, l11);
-        j3.connect(l12, l32);
-        m.addRoad(r1);
-        m.addRoad(r2);
-        m.addRoad(r3);
-        m.addJunction(j1);
-        m.addJunction(j2);
-        m.addJunction(j3);
-        s.setMap(m);
-        s.addVehicle(v1);
-        s.addVehicle(v2);
-        s.addVehicle(v3);
         BorderPane root = new BorderPane();
         //Group root = new Group();
         Canvas canvas = new Canvas(400,400);
@@ -116,16 +65,24 @@ public class GUIsimulation extends Application {
     }
     
     public void drawRoad(GraphicsContext gc){
+      
+      // SHOULD NOT DRAW EVERY SINGLE ROAD SEPARATELY
+      // IT SHOULD DRAW THE ROADS IN A LOOP DYNAMICALLY
+      
         /* set road width */
         gc.setLineWidth(2);
         /*draw road1 */
-        gc.strokeLine(r1.getStartPoint().getX(), r1.getStartPoint().getY(), r1.getEndPoint().getX(), r1.getEndPoint().getY());
+//        gc.strokeLine(r1.getStartPoint().getX(), r1.getStartPoint().getY(), r1.getEndPoint().getX(), r1.getEndPoint().getY());
         /*draw road3 */
-        gc.strokeLine(r2.getStartPoint().getX(), r2.getStartPoint().getY(), r2.getEndPoint().getX(), r2.getEndPoint().getY());
+//        gc.strokeLine(r2.getStartPoint().getX(), r2.getStartPoint().getY(), r2.getEndPoint().getX(), r2.getEndPoint().getY());
         /*draw road3 */
-        gc.strokeLine(r3.getStartPoint().getX(), r3.getStartPoint().getY(), r3.getEndPoint().getX(), r3.getEndPoint().getY());
+//        gc.strokeLine(r3.getStartPoint().getX(), r3.getStartPoint().getY(), r3.getEndPoint().getX(), r3.getEndPoint().getY());
     }
     public void drawCar(final GraphicsContext gc){
+      
+      
+      // SHOULD NOT DRAW EVERY SINGLE CAR SEPARATELY
+      // IT SHOULD DRAW THE CARS IN A LOOP DYNAMICALLY
         
         Platform.runLater(new Runnable(){
             @Override
@@ -134,20 +91,20 @@ public class GUIsimulation extends Application {
                 /* set road width */
         gc.setLineWidth(2);
         /*draw road1 */
-        gc.strokeLine(r1.getStartPoint().getX(), r1.getStartPoint().getY(), r1.getEndPoint().getX(), r1.getEndPoint().getY());
+//        gc.strokeLine(r1.getStartPoint().getX(), r1.getStartPoint().getY(), r1.getEndPoint().getX(), r1.getEndPoint().getY());
         /*draw road3 */
-        gc.strokeLine(r2.getStartPoint().getX(), r2.getStartPoint().getY(), r2.getEndPoint().getX(), r2.getEndPoint().getY());
+//        gc.strokeLine(r2.getStartPoint().getX(), r2.getStartPoint().getY(), r2.getEndPoint().getX(), r2.getEndPoint().getY());
         /*draw road3 */
-        gc.strokeLine(r3.getStartPoint().getX(), r3.getStartPoint().getY(), r3.getEndPoint().getX(), r3.getEndPoint().getY());
+//        gc.strokeLine(r3.getStartPoint().getX(), r3.getStartPoint().getY(), r3.getEndPoint().getX(), r3.getEndPoint().getY());
         /* create car1 */
                 gc.setFill(Color.BLUE);
-                gc.fillOval(v1.getPosition().getX()-(CAR_WIDTH/2), v1.getPosition().getY()-(CAR_HEIGHT/2), CAR_WIDTH, CAR_HEIGHT);
+//                gc.fillOval(v1.getPosition().getX()-(CAR_WIDTH/2), v1.getPosition().getY()-(CAR_HEIGHT/2), CAR_WIDTH, CAR_HEIGHT);
         /* create car2 */
                 gc.setFill(Color.RED);
-                gc.fillOval(v2.getPosition().getX()-(CAR_WIDTH/2), v2.getPosition().getY()-(CAR_HEIGHT/2), CAR_WIDTH, CAR_HEIGHT);
+//                gc.fillOval(v2.getPosition().getX()-(CAR_WIDTH/2), v2.getPosition().getY()-(CAR_HEIGHT/2), CAR_WIDTH, CAR_HEIGHT);
         /* create car3 */
                 gc.setFill(Color.GREEN);
-                gc.fillOval(v3.getPosition().getX()-(CAR_WIDTH/2), v3.getPosition().getY()-(CAR_HEIGHT/2), CAR_WIDTH, CAR_HEIGHT);
+//                gc.fillOval(v3.getPosition().getX()-(CAR_WIDTH/2), v3.getPosition().getY()-(CAR_HEIGHT/2), CAR_WIDTH, CAR_HEIGHT);
             }
         });
     }
