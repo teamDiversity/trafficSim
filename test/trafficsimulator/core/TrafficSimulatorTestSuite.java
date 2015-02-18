4ccec0c6f1c2d3b00db77d4a422af7b2dc143d8c
Snorrihann <snorrih@gmail.com>
2015-02-18 19:56:42 +0000

    More tests on vehicle and road system and a test suite class created

---
+++ test/trafficsimulator/core/TrafficSimulatorTestSuite.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trafficsimulator.core;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+//JUnit Suite Test
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ 
+  RoadSystemTest.class,
+  VehicleTest.class
+})
+
+/**
+ *
+ * @author snorri
+ */
+public class TrafficSimulatorTestSuite {  
+}
